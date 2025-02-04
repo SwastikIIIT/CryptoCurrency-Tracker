fbb40508af8d61ec02e109d25b76475aa56d4c96
Swastik2005 <swastikdevil123@gmail.com>
2025-02-05 00:06:10 +0530

    commit

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,46 +1,35 @@
-import { createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoHeaders={
-	 'x-rapidapi-key': '1889703e1fmsh462f32ef77b3fcbp1b58c5jsn0f9fa04cec19',
-   'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-}
+const cryptoHeaders = {
+  'x-rapidapi-key': 'd5be05a4camsh2ad67d45ecaacdap157783jsn2fc82a24ebf1',
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+};
 
-const baseURL="https://coinranking1.p.rapidapi.com";
+const baseURL = "https://coinranking1.p.rapidapi.com";
 
-const createRequest=(url)=>{
-    return {url,headers:cryptoHeaders}
-}
+const createRequest = (url) => ({
+  url,
+  headers: cryptoHeaders,
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+  },
+});
+
 export const cryptoApi = createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery: fetchBaseQuery({baseURL}),
-    endpoints: (builder) => ({
-      getCryptos: builder.query({
-        query: () => "/",
-      }),
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: baseURL,
+    prepareHeaders: (headers) => {
+      headers.set('x-rapidapi-key', cryptoHeaders['x-rapidapi-key']);
+      headers.set('x-rapidapi-host', cryptoHeaders['x-rapidapi-host']);
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/stats"),
     }),
-  })
+  }),
+});
 
-
-export const{useGetCryptosQuery}=cryptoApi;
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/stats',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl'
-//   },
-//   headers: {
-//     'x-rapidapi-key': '1889703e1fmsh462f32ef77b3fcbp1b58c5jsn0f9fa04cec19',
-//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-//   }
-// };
-// const options = {
-// 	method: 'GET',
-// 	hostname: 'coinranking1.p.rapidapi.com',
-// 	port: null,
-// 	path: '/stats?referenceCurrencyUuid=yhjMzLPhuIDl',
-// 	headers: {
-// 		'x-rapidapi-key': 'd5be05a4camsh2ad67d45ecaacdap157783jsn2fc82a24ebf1',
-// 		'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
-// 	}
-// };
+export const { useGetCryptosQuery } = cryptoApi;
